9c611646cf0e36c960868fc61cec293deeae6b2c
Justin Cordova <justcord10@gmail.com>
2023-11-08 16:54:03 -0800

    set up the server and set up the skeleton for flow of data to the incoming get and post requests and just need to link to the DB

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,31 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the axios module to request repos for a specific
-  // user from the github API
+//the callback in this case is our save function in index.js
+let getReposByUsername = (username, callback) => {
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
-  let options = {
-    url: 'FILL ME IN',
+  const options = {
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `${config.TOKEN}`
     }
   };
 
-}
+  // TODO - Use the axios module to request repos for a specific
+  // user from the github API
+  axios.get(options.url, {headers: options.headers})
+    .then((res) => {
+      // console.log('We enter this');
+      // console.log(res.data);
+      callback(null, res.data)
+    })
+    .catch((err) => {
+      callback(err, null)
+    });
+
+};
 
 module.exports.getReposByUsername = getReposByUsername;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,43 @@
 const express = require('express');
+const axios = require('axios');
+const path = require('path'); //path is a node core module so that we can use path.join.
+const github = require('../helpers/github');
+const save = require('../database/index');
 let app = express();
 
+app.use(express.json()); //so that our requests are parsed properly
+app.use(express.static(path.join(__dirname, '../client/dist'))); //so that our static bundled files are compiled properties. __dirname is the pathname to the directory this file lives in
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
 // Webpack is configured to generate files in that directory and
 // this server must serve those files when requested.
 
+//these are like the event handlers from the last one/////////////////////////////////////////////////////////////////
+
+// This route should take the github username provided
+// and get the repo information from the github API, then
+// save the repo information in the database
+
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+  console.log('did this come through?  ', req.body); //confirmed sent through
+  //res.status(200).send(req.body.username);
+  github.getReposByUsername(req.body.username, (err, data) => {
+    if (err) {
+      res.status(400).json(err);
+    } else {
+    //in here i would call the Save function with another callback that sets the res.body and returns on success
+      save.save(data, (data) => {
+        res.status(201).json(data);
+      });
+
+    }
+  });
 });
 
 app.get('/repos', function (req, res) {
+  //need to acess req.body(or req.data)
+  console.log('in da GET');
+  res.status(200).send(['hello world']);
   // TODO - your code here!
   // This route should send back the top 25 repos
 });
